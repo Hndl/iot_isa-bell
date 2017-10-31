e6c51f5bd770b3fc3c2bde19c2c463fd2b58113d
Hndl <clive.bury@gmail.com>
2017-10-31 20:20:13 +0000

    IMP-001
    Added working days (be sure to provide on cmd line when running node app mon,tue,wed,thur,fri 9:00-18:00
    Added working hours too ( ensure 2nd param is hours of working, see above)
    Removed the aws security to separate file. I'll add an example back into GIT for completeness.

--- app.js
+++ app.js
@@ -2,14 +2,16 @@
 
 
 const aws 		= require('aws-sdk');
-const player		= require('play-sound')();
+const player	= require('play-sound')();
 const util 		= require('util');
-const readline		= require('readline');
+const readline	= require('readline');
 const path		= require('path');
 const fs 		= require('fs');
 const exec		= require('child_process').exec;
 
-const CMDLINE_AUTOPLAY		=	'defaults write com.apple.QuickTimePlayerX MGPlayMovieOnOpen 1';
+
+const DOTW				=	['sun','mon','tue','wed','thur','fri','sat'];
+const CMDLINE_AUTOPLAY	=	'defaults write com.apple.QuickTimePlayerX MGPlayMovieOnOpen 1';
 const CMDLINE_AUTO 		=	'open -F ';
 const LOG_TYPE			=	['[inf]','[err]','[war]'];
 const LOG_INF			=	0;
@@ -20,23 +22,27 @@ const CURR_DIR			=	".";
 const CLICK 			= 	['single','double'];
 const SINGLE 			=	0;
 const DOUBLE 			=	1;
-
-const EVT 			= ['SIGINT'];
+const COMMA_DELIM		=	',';
+const HYPHEN_DELIM		=	'-';
+const EVT 				= ['SIGINT'];
 const EVT_FS_SIGINT		= 0;
-const SQS_OPTIONS		= 	{/* usr: iotButton */
-				"accessKeyId"			: "", 
-			 	"secretAccessKey"		: "", 
-			 	"region"			: "eu-west-1"
-					};
+const WORKDAY_TRUE		= 99;
+
+// IMP:[001] removed the security options and placed them in a json file.  Same format. Read in using aws.config.loadFromPath
+//const SQS_OPTIONS		= 	
+//
+const AWS_CRED_FILE		= 'aws.cred.json';
+const SQS_OPTIONS		= aws.config.loadFromPath ( path.join('.',AWS_CRED_FILE));
 
 const SQS_RECEIVE_OPT	=	{
-								"QueueUrl"				: "https://sqs.eu-west-1.amazonaws.com/?/?",
+								"QueueUrl"				: "https://sqs.eu-west-1.amazonaws.com/381605567957/irl-iot-button-click",
       							"MaxNumberOfMessages"	: 1
 							};
 
 const sqs 				=	new aws.SQS( SQS_OPTIONS );
 const SQS_SLEEP			=	1000;
 
+var WorkingDayShedule	= {};
 /**
  * func: deleteMessageSQSJSON
  * Desc: used as a simple way to construct JavaObjects that contain the relevant delete message instructions
@@ -53,6 +59,40 @@ function deleteMessageSQSJSON ( msg ){
       			ReceiptHandle: msg.ReceiptHandle
 			}
 		);
+}
+
+//IMP[002] due to the costs involved, we only want the app to work on days we are going to be in the office.
+function isTimeToWork ( startTime, endTime){
+	const HH=0;
+	const MM=1;
+	const currentHH = parseInt(((new Date()).getHours()));
+	//const currentMM = parseInt(((new Date()).getMinutes()));
+	
+	var start_time = startTime.split(':');
+	var end_time   = endTime.split(':');
+
+	return ( (currentHH >= parseInt(start_time[HH]) ) && (currentHH <= parseInt(end_time[HH]) ) );
+}
+
+function isTodayAWorkingDay( listOfWorkingDays ){
+	var i = 0;
+	for ( i =0 ; i<listOfWorkingDays.length ; i++){
+
+		
+		if (workingToday ( listOfWorkingDays[i].trim().toLowerCase() )) {
+			//console.log(`checked : ${listOfWorkingDays[i].trim().toLowerCase()} - today is a working day!`);
+			i=(WORKDAY_TRUE-1);//subtract 1, as the i++ in the loop will increment.
+		}
+		
+	}
+	//console.log(`value of i=${i} workingday:${i===99}`);
+	return ( i===WORKDAY_TRUE);
+}
+function workingToday(  dayOfTheWeek ) {
+	return ( dayOfTheWeek.trim().toLowerCase() === (nameOfToday()).trim().toLowerCase() ) ;
+}
+function nameOfToday (){
+	return (DOTW[(new Date()).getDay()]);
 }
 
 function report_error( func, subject, message ){
@@ -127,7 +167,17 @@ function whichClick ( messageBody ) {
  */
 function receiveMessage() {
 	const func = "receiveMessage";
-  	report_info(func,'query_sqs',SQS_RECEIVE_OPT.QueueUrl);
+  	
+
+  	/*
+  	 * should I be working? 
+  	 * // Hacking!  just check and terminate if it's not a working day!
+  	 */
+  	 if ( ( (isTodayAWorkingDay(WorkingDayShedule))===false) && (isTimeToWork(WorkingHours[0],WorkingHours[1]) === false) ){
+  	 	util.log(`No work on ${nameOfToday()}. The working shedule is:${WorkingDayShedule}`);
+  	 	return;
+  	 } 
+  	 report_info(func,'query_sqs',SQS_RECEIVE_OPT.QueueUrl);
 	
 	/*
 	 * Invoke receiveMessage to check for new messages on our queue.
@@ -174,6 +224,12 @@ function receiveMessage() {
 
 
 cmdline (CMDLINE_AUTOPLAY);
+
+var WorkingDayShedule = process.argv[2].split(COMMA_DELIM);
+var WorkingHours = process.argv[3].split(HYPHEN_DELIM);
+//console.log(`${WorkingHours} - ${WorkingHours[0]} - ${WorkingHours[1]}`);
+
+
 
 var button_click = setInterval (receiveMessage,SQS_SLEEP);
 
